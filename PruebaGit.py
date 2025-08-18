3eb8207f6b17e3ff19e07d4d6d533246af6378c0
Fabian Mejias <fabian.mejiast@outlook.com>
2025-08-18 13:08:16 -0400

    prubea

--- PruebaGit.py
+++ PruebaGit.py
@@ -1,1 +1,9 @@
 print("Primer Commit en Github")
+
+for i in range(5):
+    print(f"Iteracion {i+1}")
+
+# Esta carpeta es una prueba para el proyecto de
+# IA en IdS, con una aplicación con algoritmos Geneticos. 
+
+print("Cambie el nombre del repositorio")
