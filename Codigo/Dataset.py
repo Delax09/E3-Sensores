9e746dc2754b05e1751ff842522d8a0140e297b4
Fabian Mejias <fabian.mejiast@outlook.com>
2025-09-15 12:53:08 -0300

    Forma del Dataset final

--- Codigo/Dataset.py
+++ Codigo/Dataset.py
@@ -1,67 +1,31 @@
-"""
-Este archivo es de prueba para el modelo de  datos que vamos a utilizar
-"""
+interseccion00 = {
+    "Tipo_congestion": 1,
+    "Tipo_contaminacion": 1,
+    "Demanda_estacionamiento": 1
+}
+interserccion01 = {
+    "Tipo_congestion": 2,
+    "Tipo_contaminacion": 2,
+    "Demanda_estacionamiento": 2
+    }
+
+interseccion10 ={
+    "Tipo_congestion": 3,
+    "Tipo_contaminacion": 3,
+    "Demanda_estacionamiento": 3
+    }
 
 interseccion11 = {
-    "ID": "11",
-    "Corrdenadas": (1,1),
-    "Tipo_zona": 1,
-    "Tipo_congestion": 3,
-    "Tipo_contaminacion": 2,
-    "Demanda_estacionamiento": 3,
-    "Sensor_trafico": True,
-    "Sensor_aire": True,
-    "Sensor_estacionamiento": False,
-    "Datos": {
-        "Flujo_vehicular": 120,
-        "PM2.5": 35,
-        "NO2": 22,
-        "Ocupacion_estacionamiento": None
-        }
-}
-
-'''
-Eliminar la variable "Costo_sensor" y "Cobertura"
-'''
-
-#insterseccion11["Costo_sensor"] = 1500 #Actualizando un valor
-
-mapa_ciudad = [[interseccion00, interseccion01, interseccion02], 
-               [interseccion10, interseccion11, interseccion12],
-               [interseccion20, interseccion21, interseccion22]]
-
-#Asi hay que recorrer el diccionario
-for clave, valor in interseccion11.items():
-    #Diccionario anidado
-    if clave == "Datos":
-        print(clave)
-        for clave2, valor2 in valor.items():
-            print("  ", clave2, valor2)
-    else:
-        print(clave, valor)
-
-#Esto es para que sea m�s intuitivo para el algoritmo
-#para nosotros tener una idea de los datos y el modelado
-zonas = {
-    "Comercial" : 1,
-    "Residencial": 2,
-    "Empresarial": 3
+    "Tipo_congestion": 4,
+    "Tipo_contaminacion": 4,
+    "Demanda_estacionamiento": 4
     }
 
-congestion = {
-    "Baja": 1,
-    "Media": 2,
-    "Alta": 3
-    }
+calles = [interseccion00, interserccion01,
+          interseccion10, interseccion11]
+"""
+Sensor_Trafico, Aire, Estacionamiento = que lo decida el algoritmo
+Costos definida por el usuario
+"""
 
-contaminacion = {
-    "Baja": 1,
-    "Media": 2,
-    "Alta": 3
-    }
 
-demanda_estacionamiento = {
-    "Baja": 1,
-    "Media": 2,
-    "Alta": 3
-    }
