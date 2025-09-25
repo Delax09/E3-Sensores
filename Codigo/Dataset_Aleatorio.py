d29e798a72d4334ca9b219bf4868462ccda1322d
Fabian Mejias <fabian.mejiast@outlook.com>
2025-09-24 23:44:10 -0300

    Cambie nombre de archivo para hacer pruebas

---
+++ Codigo/Dataset_Aleatorio.py
@@ -0,0 +1,69 @@
+from random import randint
+
+interseccion00 = {
+    "Id": "00",
+    "Tipo_congestion": randint(1,3),
+    "Tipo_contaminacion": randint(1,3),
+    "Demanda_estacionamiento": randint(1,3)
+}
+interserccion01 = {
+    "Id": "01",
+    "Tipo_congestion": randint(1,3),
+    "Tipo_contaminacion": randint(1,3),
+    "Demanda_estacionamiento": randint(1,3)
+}
+interseccion02 = {
+    "Id": "02",
+    "Tipo_congestion": randint(1,3),
+    "Tipo_contaminacion": randint(1,3),
+    "Demanda_estacionamiento": randint(1,3)
+}
+
+interseccion10 ={
+    "Id": "10",
+    "Tipo_congestion": randint(1,3),
+    "Tipo_contaminacion": randint(1,3),
+    "Demanda_estacionamiento": randint(1,3)
+    }
+
+interseccion11 = {
+    "Id": "11",
+    "Tipo_congestion": randint(1,3),
+    "Tipo_contaminacion": randint(1,3),
+    "Demanda_estacionamiento": randint(1,3)
+    }
+
+interseccion12 = {
+    "Id": "12",
+    "Tipo_congestion": randint(1,3),
+    "Tipo_contaminacion": randint(1,3),
+    "Demanda_estacionamiento": randint(1,3)
+}
+interseccion20 = {
+    "Id": "20",
+    "Tipo_congestion": randint(1,3),
+    "Tipo_contaminacion": randint(1,3),
+    "Demanda_estacionamiento": randint(1,3)
+}
+interseccion21 = {
+    "Id": "21",
+    "Tipo_congestion": randint(1,3),
+    "Tipo_contaminacion": randint(1,3),
+    "Demanda_estacionamiento": randint(1,3)
+}
+interseccion22 = {
+    "Id": "22",
+    "Tipo_congestion": randint(1,3),
+    "Tipo_contaminacion": randint(1,3),
+    "Demanda_estacionamiento": randint(1,3)
+}
+
+calles = [interseccion00, interserccion01,interseccion02,
+          interseccion10, interseccion11, interseccion12,
+          interseccion20, interseccion21, interseccion22]
+"""
+Sensor_Trafico, Aire, Estacionamiento = que lo decida el algoritmo
+Costos definida por el usuario
+"""
+
+
