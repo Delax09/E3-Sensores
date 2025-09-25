76fa50a38b4a189458a0be76d5ac0cea47966643
Fabian Mejias <fabian.mejiast@outlook.com>
2025-09-24 23:44:25 -0300

    Hice un dataset predefinido para probar

---
+++ Codigo/Dataset_Predefinido.py
@@ -0,0 +1,63 @@
+interseccion00 = {
+    "Id": "00",
+    "Tipo_congestion": 1,
+    "Tipo_contaminacion": 1,
+    "Demanda_estacionamiento": 3
+}
+interserccion01 = {
+    "Id": "01",
+    "Tipo_congestion": 3,
+    "Tipo_contaminacion": 1,
+    "Demanda_estacionamiento": 1
+}
+interseccion02 = {
+    "Id": "02",
+    "Tipo_congestion": 1,
+    "Tipo_contaminacion": 3,
+    "Demanda_estacionamiento": 1
+}
+
+interseccion10 ={
+    "Id": "10",
+    "Tipo_congestion": 1,
+    "Tipo_contaminacion": 2,
+    "Demanda_estacionamiento": 3
+    }
+
+interseccion11 = {
+    "Id": "11",
+    "Tipo_congestion": 2,
+    "Tipo_contaminacion":3,
+    "Demanda_estacionamiento": 1
+    }
+
+interseccion12 = {
+    "Id": "12",
+    "Tipo_congestion": 3,
+    "Tipo_contaminacion": 2,
+    "Demanda_estacionamiento": 1
+}
+interseccion20 = {
+    "Id": "20",
+    "Tipo_congestion": 3,
+    "Tipo_contaminacion": 3,
+    "Demanda_estacionamiento": 3
+}
+interseccion21 = {
+    "Id": "21",
+    "Tipo_congestion": 1,
+    "Tipo_contaminacion": 1,
+    "Demanda_estacionamiento": 1
+}
+interseccion22 = {
+    "Id": "22",
+    "Tipo_congestion": 1,
+    "Tipo_contaminacion": 1,
+    "Demanda_estacionamiento":1
+}
+
+calles = [interseccion00, interserccion01,interseccion02,
+          interseccion10, interseccion11, interseccion12,
+          interseccion20, interseccion21, interseccion22]
+
+
